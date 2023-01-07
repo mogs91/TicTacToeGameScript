21ecef056b5e59dfb50333f6028a4d8c37e08172
Mogs91 <markossayan@gmail.com>
2023-01-07 13:58:22 +0100

    Game completed , need to make the executable now.

--- main.py
+++ main.py
@@ -65,7 +65,8 @@ def has_won(game_board):
             or game_board[0][2] == 'O'and game_board[1][1] == 'O' and game_board[2][0] == 'O':
         game_over = True
         return game_over
-
+    else:
+        return game_over
 
 print("Let's play TicTacToe!! \n"
       "Player X will go first and then Player O\n"
@@ -79,27 +80,52 @@ is_game_running = True
 player_X_turn = True
 player_O_turn = False
 
-while is_game_running:
-    print("Please tell us the row and the column you want to write in")
-    if player_X_turn:
-        player_row = int(input('Player X Please enter row you want to mark on the board: '))
-        player_col = int(input('Player X Please enter column you want to mark on the board: '))
-        game_board[check_row(player_row)][check_column(player_col)] = 'X'
-        for row in game_board:
-            print(row)
-        player_O_turn = True
-    if player_O_turn:
-        player_row = int(input('Player O Please enter row you want to mark on the board: '))
-        player_col = int(input('Player O Please enter column you want to mark on the board: '))
-        game_board[check_row(player_row)][check_column(player_col)] = 'O'
-        for row in game_board:
-            print(row)
+def play_game(is_game_running, player_X_turn, player_O_turn,game_board):
+    while is_game_running:
+        game_over = has_won(game_board)
+        if game_over:
+            print("GAME OVER Player O wins!")
+            is_game_running = False
+            break
+        else:
+            print("Please tell us the row and the column you want to write in")
+            if player_X_turn:
+                player_row = int(input('Player X Please enter row you want to mark on the board: '))
+                player_col = int(input('Player X Please enter column you want to mark on the board: '))
+                game_board[check_row(player_row)][check_column(player_col)] = 'X'
+                for row in game_board:
+                    print(row)
+                game_over = has_won(game_board)
+                print(game_over)
+                if game_over:
+                    print("GAME OVER Player X wins!")
+                    is_game_running = False
+                    break
+                else:
+                    player_O_turn = True
+            if player_O_turn:
+                player_row = int(input('Player O Please enter row you want to mark on the board: '))
+                player_col = int(input('Player O Please enter column you want to mark on the board: '))
+                game_board[check_row(player_row)][check_column(player_col)] = 'O'
+                for row in game_board:
+                    print(row)
+                player_X_turn = True
+
+
+if is_game_running:
+    play_game(is_game_running, player_X_turn, player_O_turn)
+elif is_game_running== False:
+    print("Thank you for playing")
+    restart = input("If want to play again press Y else press N: ")
+    if restart == 'Y':
+        game_board = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]
+        is_game_running = True
         player_X_turn = True
-    has_won(game_board)
-    if has_won:
-        print("the game is over player wins")
-
-
+        player_O_turn = False
+        play_game(is_game_running, player_X_turn, player_O_turn)
+    elif restart == 'N':
+        print("Thank you for playing")
+        exit()
 
 
 
